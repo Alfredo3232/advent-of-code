481511f9c0fd86b58877cd97735e3979b904c3d1
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2024-12-04 10:11:54 -0800

    Add day 2

---
+++ 2024/day-2/one.js
@@ -0,0 +1,37 @@
+let safeCounter = 0;
+
+for (let i = 0; i < arr.length; i++) {
+    let diffArr = [];
+    let boo = true;
+
+    for (let j = 1; j < arr[i].length; j++) {
+        let diff = arr[i][j] - arr[i][j - 1];
+
+        if (diff > 3 || diff < -3 || diff === 0) {
+            boo = false;
+            break;
+        } else diffArr.push(diff);
+    }
+
+    if (boo) {
+        for (let j = 0; j < diffArr.length; j++) {
+            let isInc = diffArr[0] > 0;
+
+            if (isInc) {
+                if (!(diffArr[j] > 0)) {
+                    boo = false;
+                    break;
+                }
+            } else {
+                if (!(diffArr[j] < 0)) {
+                    boo = false;
+                    break;
+                }
+            }
+        }
+
+        if (boo) safeCounter++;
+    } else continue;
+}
+
+console.log("Safe --------->", safeCounter);
